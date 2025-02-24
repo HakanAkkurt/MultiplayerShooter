dbc174b5acebd965ff3632e2c279ea7106ae8208
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2025-02-24 19:54:14 +0100

    Match timer added

--- Source/MultiplayerShooter/PlayerController/MS_PlayerController.cpp
+++ Source/MultiplayerShooter/PlayerController/MS_PlayerController.cpp
@@ -15,6 +15,66 @@ void AMS_PlayerController::BeginPlay()
 	MS_HUD = Cast<AMS_HUD>(GetHUD());
 }
 
+void AMS_PlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetHUDTime();
+
+	CheckTimeSync(DeltaTime);
+}
+
+void AMS_PlayerController::CheckTimeSync(float DeltaTime)
+{
+	TimeSyncRunningTime += DeltaTime;
+
+	if (IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency) {
+
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+		TimeSyncRunningTime = 0.f;
+	}
+}
+
+float AMS_PlayerController::GetServerTime()
+{
+	if (HasAuthority()) return GetWorld()->GetTimeSeconds();
+	else return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+}
+
+void AMS_PlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+
+	if (IsLocalController()) {
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+	}
+}
+
+void AMS_PlayerController::SetHUDTime()
+{
+	uint32 SecondsLeft = FMath::CeilToInt(MatchTime - GetServerTime());
+
+	if (CountdownInt != SecondsLeft) {
+
+		SetHUDMatchCountdown(MatchTime - GetServerTime());
+	}
+	CountdownInt = SecondsLeft;
+}
+
+void AMS_PlayerController::ServerRequestServerTime_Implementation(float TimeOfClientRequest)
+{
+	float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+	ClientReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
+}
+
+void AMS_PlayerController::ClientReportServerTime_Implementation(float TimeOfClientRequest, float TimeServerReceivedClientRequest)
+{
+	float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfClientRequest;
+	float CurrentServerTime = TimeServerReceivedClientRequest + (0.5f * RoundTripTime);
+
+	ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
+}
+
 void AMS_PlayerController::SetHUDHealth(float Health, float MaxHealth)
 {
 	MS_HUD = MS_HUD == nullptr ? Cast<AMS_HUD>(GetHUD()) : MS_HUD;
@@ -67,6 +127,19 @@ void AMS_PlayerController::SetHUDCarriedAmmo(int32 Ammo)
 
 		FString AmmoText = FString::Printf(TEXT("%d"), Ammo);
 		MS_HUD->CharacterOverlay->CarriedAmmoAmount->SetText(FText::FromString(AmmoText));
+	}
+}
+
+void AMS_PlayerController::SetHUDMatchCountdown(float CountdownTime)
+{
+	MS_HUD = MS_HUD == nullptr ? Cast<AMS_HUD>(GetHUD()) : MS_HUD;
+	if (MS_HUD && MS_HUD->CharacterOverlay && MS_HUD->CharacterOverlay->MatchCountdownText) {
+
+		int32 Minutes = FMath::FloorToInt(CountdownTime / 60.f);
+		int32 Seconds = CountdownTime - Minutes * 60;
+
+		FString CountdownText = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+		MS_HUD->CharacterOverlay->MatchCountdownText->SetText(FText::FromString(CountdownText));
 	}
 }
 
--- Source/MultiplayerShooter/PlayerController/MS_PlayerController.h
+++ Source/MultiplayerShooter/PlayerController/MS_PlayerController.h
@@ -21,14 +21,44 @@ public:
 	void SetHUDDefeats(int32 Defeats);
 	void SetHUDWeaponAmmo(int32 Ammo);
 	void SetHUDCarriedAmmo(int32 Ammo);
+	void SetHUDMatchCountdown(float CountdownTime);
 
 	virtual void OnPossess(APawn* InPawn) override;
 
+	virtual void Tick(float DeltaTime) override;
+
+	virtual float GetServerTime(); // Synced with server world clock
+
+	virtual void ReceivedPlayer() override; // Synced with server clock as soon as possible
+
 protected:
 	virtual void BeginPlay() override;
+
+	void SetHUDTime();
+
+	// Sync time between client and server
+
+	// Request the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+
+	// Reports the current server time to the client in repsonse to ServerRequestServerTime
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
+	float ClientServerDelta = 0.f; // difference between client and server time
+
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 5.f;
+
+	float TimeSyncRunningTime = 0.f;
+
+	void CheckTimeSync(float DeltaTime);
 
 private:
 	UPROPERTY()
 	class AMS_HUD* MS_HUD;
 
+	float MatchTime = 120.f;
+	uint32 CountdownInt = 0;
 };
