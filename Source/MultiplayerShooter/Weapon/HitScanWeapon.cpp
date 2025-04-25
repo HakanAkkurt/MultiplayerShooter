4fc9a22c0cbd16dba1676cfa2454a4ea5f15957c
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2025-04-25 19:39:43 +0200

    Weapon Scatter

--- Source/MultiplayerShooter/Weapon/HitScanWeapon.cpp
+++ Source/MultiplayerShooter/Weapon/HitScanWeapon.cpp
@@ -25,84 +25,92 @@ void AHitScanWeapon::Fire(const FVector& HitTarget)
 
 		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
 		FVector Start = SocketTransform.GetLocation();
-		FVector End = Start + (HitTarget - Start) * 1.25f;
 
 		FHitResult FireHit;
-		UWorld* World = GetWorld();
-		if (World) {
 
-			World->LineTraceSingleByChannel(
-				FireHit,
-				Start,
-				End,
-				ECollisionChannel::ECC_Visibility
+		WeaponTraceHit(Start, HitTarget, FireHit);
+
+		APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(FireHit.GetActor());
+
+		if (PlayerCharacter && HasAuthority() && InstigatorController) {
+
+			UGameplayStatics::ApplyDamage(
+				PlayerCharacter,
+				Damage,
+				InstigatorController,
+				this,
+				UDamageType::StaticClass()
 			);
-
-			FVector BeamEnd = End;
-			if (FireHit.bBlockingHit) {
-
-				BeamEnd = FireHit.ImpactPoint;
-				APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(FireHit.GetActor());
-
-				if (PlayerCharacter && HasAuthority() && InstigatorController) {
-
-					UGameplayStatics::ApplyDamage(
-						PlayerCharacter,
-						Damage,
-						InstigatorController,
-						this,
-						UDamageType::StaticClass()
-					);
-				}
-
-				if (ImpactParticles) {
-
-					UGameplayStatics::SpawnEmitterAtLocation(
-						World,
-						ImpactParticles,
-						FireHit.ImpactPoint,
-						FireHit.ImpactNormal.Rotation()
-					);
-				}
-
-				if (HitSound) {
-					UGameplayStatics::PlaySoundAtLocation(
-						this,
-						HitSound,
-						FireHit.ImpactPoint
-					);
-				}
-
-				if (BeamParticles) {
-					UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(
-						World,
-						BeamParticles,
-						SocketTransform
-					);
-					if (Beam) {
-						Beam->SetVectorParameter(FName("Target"), BeamEnd);
-					}
-				}
-
-			}
-
-			if (MuzzleFlash) {
-				UGameplayStatics::SpawnEmitterAtLocation(
-					World,
-					MuzzleFlash,
-					SocketTransform
-				);
-			}
-
-			if (FireSound) {
-				UGameplayStatics::PlaySoundAtLocation(
-					this,
-					FireSound,
-					GetActorLocation()
-				);
-			}
 		}
 
+		if (ImpactParticles) {
+
+			UGameplayStatics::SpawnEmitterAtLocation(
+				GetWorld(),
+				ImpactParticles,
+				FireHit.ImpactPoint,
+				FireHit.ImpactNormal.Rotation()
+			);
+		}
+
+		if (HitSound) {
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				HitSound,
+				FireHit.ImpactPoint
+			);
+		}
+
+		FVector End = Start + (HitTarget - Start) * 1.25f;
+
+		if (MuzzleFlash) {
+			UGameplayStatics::SpawnEmitterAtLocation(
+				GetWorld(),
+				MuzzleFlash,
+				SocketTransform
+			);
+		}
+
+		if (FireSound) {
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				FireSound,
+				GetActorLocation()
+			);
+		}
+	}
+}
+
+void AHitScanWeapon::WeaponTraceHit(const FVector& TraceStart, const FVector& HitTarget, FHitResult& OutHit)
+{
+	UWorld* World = GetWorld();
+	if (World) {
+
+		FVector End = bUseScatter ? TraceEndWithScatter(TraceStart, HitTarget) : TraceStart + (HitTarget - TraceStart) * 1.25f;
+		World->LineTraceSingleByChannel(
+			OutHit,
+			TraceStart,
+			End,
+			ECollisionChannel::ECC_Visibility
+		);
+
+		FVector BeamEnd = End;
+		if (OutHit.bBlockingHit) {
+			BeamEnd = OutHit.ImpactPoint;
+		}
+
+		if (BeamParticles) {
+			UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(
+				World,
+				BeamParticles,
+				TraceStart,
+				FRotator::ZeroRotator,
+				true
+			);
+			if (Beam) {
+				Beam->SetVectorParameter(FName("Target"), BeamEnd);
+			}
+		}
 	}
 }
 
@@ -114,31 +122,31 @@ FVector AHitScanWeapon::TraceEndWithScatter(const FVector& TraceStart, const FVe
 	FVector EndLoc = SphereCenter + RandVec;
 	FVector ToEndLoc = EndLoc - TraceStart;
 
-	DrawDebugSphere(
-		GetWorld(),
-		SphereCenter,
-		SphereRadius,
-		12,
-		FColor::Red,
-		true
-	);
+	//DrawDebugSphere(
+	//	GetWorld(),
+	//	SphereCenter,
+	//	SphereRadius,
+	//	12,
+	//	FColor::Red,
+	//	true
+	//);
 
-	DrawDebugSphere(
-		GetWorld(),
-		EndLoc,
-		4.f,
-		12,
-		FColor::Orange,
-		true
-	);
+	//DrawDebugSphere(
+	//	GetWorld(),
+	//	EndLoc,
+	//	4.f,
+	//	12,
+	//	FColor::Orange,
+	//	true
+	//);
 
-	DrawDebugLine(
-		GetWorld(),
-		TraceStart,
-		FVector(TraceStart + ToEndLoc * TRACE_LENGTH / ToEndLoc.Size()),
-		FColor::Green,
-		true
-	);
+	//DrawDebugLine(
+	//	GetWorld(),
+	//	TraceStart,
+	//	FVector(TraceStart + ToEndLoc * TRACE_LENGTH / ToEndLoc.Size()),
+	//	FColor::Green,
+	//	true
+	//);
 
 	return FVector(TraceStart + ToEndLoc * TRACE_LENGTH / ToEndLoc.Size());
 }
