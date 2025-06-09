a6a44e2651997600127aa1facebe976f260eecdd
HakanAkkurt <Hakan_Akkurt@hotmail.de>
2025-06-09 20:26:55 +0200

    Health Pickup added

---
+++ Source/MultiplayerShooter/Pickups/HealthPickup.cpp
@@ -0,0 +1,44 @@
+// Hakan Games. All Rights Reserved.
+
+
+#include "HealthPickup.h"
+#include "MultiPlayerShooter/Character/PlayerCharacter.h"
+#include "MultiPlayerShooter/Components/BuffComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(OtherActor);
+	if (PlayerCharacter) {
+
+
+
+	}
+
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{
+	if (PickupEffect) {
+
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			PickupEffect,
+			GetActorLocation(),
+			GetActorRotation()
+		);
+	}
+
+	Super::Destroyed();
+}
---
+++ Source/MultiplayerShooter/Pickups/HealthPickup.h
@@ -0,0 +1,46 @@
+// Hakan Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "HealthPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSHOOTER_API AHealthPickup : public APickup
+{
+	GENERATED_BODY()
+
+public:
+	AHealthPickup();
+
+	virtual void Destroyed() override;
+
+protected:
+
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	float HealAmount = 100.f;
+
+	UPROPERTY(EditAnywhere)
+	float HealingTime = 5.f;
+	
+	UPROPERTY(VisibleAnywhere)
+	class UNiagaraComponent* PickupEffectComponent;
+
+	UPROPERTY(EditAnywhere)
+	class UNiagaraSystem* PickupEffect;
+};
